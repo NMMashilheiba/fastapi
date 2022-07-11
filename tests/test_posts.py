731d0ef05b586164bdb3876f956d273601bacada
Mashilheiba <gdragonnm@gmail.com>
2022-07-11 20:14:01 +0530

    add test_detele

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -63,3 +63,16 @@ def test_create_post_default_published_true(authorized_client, test_user, test_p
 def test_unauthorized_user_create_post(client, test_user, test_posts):
     res = client.post("/posts/", json={"title": "titleapp", "content": "content apple"})
     assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_Post(client, test_user, test_posts):
+    res = client.delete(
+        f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_post_success(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(
+        f"/posts/{test_posts[0].id}")
+
+    assert res.status_code == 204
